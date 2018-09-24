77c7e1495192f6c9d9f9578aeb744eb914dfbe9d
asaf <asaf@flexi-core.com>
2018-09-24 12:24:52 +0300

    1.0.3 added apiprovider for later use , moved filtering related object here as they can be persisted , moved inspectEquipmentRequest here as it is also persistable (both by scheduling ) , added relevant annotations on filters , removed canonical class name as it is the same as the newly added return type

--- src/main/java/com/flexicore/product/model/EquipmentGroupFiltering.java
+++ src/main/java/com/flexicore/product/model/EquipmentGroupFiltering.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 @Entity
 public class EquipmentGroupFiltering extends EquipmentFiltering {
 
-    @FieldInfo(displayName = "precision",description = "geo hash percision")
+    @FieldInfo(displayName = "precision",description = "geo hash precision")
     private int precision;
 
     public int getPrecision() {
