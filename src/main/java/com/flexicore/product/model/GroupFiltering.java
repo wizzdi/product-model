3a1158fdabd5e6aaeaf48d10674658670da2140a
asaf <asaf@flexi-core.com>
2018-09-24 12:42:09 +0300

    1.0.3 added apiprovider for later use , moved filtering related object here as they can be persisted , moved inspectEquipmentRequest here as it is also persistable (both by scheduling ) , added relevant annotations on filters , removed canonical class name as it is the same as the newly added return type

---
+++ src/main/java/com/flexicore/product/model/GroupFiltering.java
@@ -0,0 +1,47 @@
+package com.flexicore.product.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.flexicore.interfaces.dynamic.IdRefFieldInfo;
+import com.flexicore.model.BaseclassIdFiltering;
+import com.flexicore.model.FilteringInformationHolder;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class GroupFiltering extends FilteringInformationHolder {
+
+    @OneToMany(targetEntity = BaseclassIdFiltering.class, cascade = {CascadeType.MERGE, CascadeType.PERSIST}, mappedBy = "filteringInformationHolder")
+    @IdRefFieldInfo(displayName = "groupIds",description = "equipment group ids",refType = EquipmentGroup.class)
+    private Set<BaseclassIdFiltering> groupIds = new HashSet<>();
+    @JsonIgnore
+    @Transient
+    private List<EquipmentGroup> equipmentGroups = new ArrayList<>();
+
+    @OneToMany(targetEntity = BaseclassIdFiltering.class, cascade = {CascadeType.MERGE, CascadeType.PERSIST}, mappedBy = "filteringInformationHolder")
+    public Set<BaseclassIdFiltering> getGroupIds() {
+        return groupIds;
+    }
+
+    public GroupFiltering setGroupIds(Set<BaseclassIdFiltering> groupIds) {
+        this.groupIds = groupIds;
+        return this;
+    }
+
+    @JsonIgnore
+    @Transient
+    public List<EquipmentGroup> getEquipmentGroups() {
+        return equipmentGroups;
+    }
+
+    public GroupFiltering setEquipmentGroups(List<EquipmentGroup> equipmentGroups) {
+        this.equipmentGroups = equipmentGroups;
+        return this;
+    }
+}
