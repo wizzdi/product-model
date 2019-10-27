9a782c8cf10d5b218c4afde839785d85e250b9ef
asaf <asaf@flexi-core.com>
2019-10-27 09:53:44 +0200

    1.2.20

--- src/main/java/com/flexicore/product/model/ProductTypeFiltering.java
+++ src/main/java/com/flexicore/product/model/ProductTypeFiltering.java
@@ -1,6 +1,7 @@
 package com.flexicore.product.model;
 
 
+import com.flexicore.interfaces.dynamic.FieldInfo;
 import com.flexicore.model.FilteringInformationHolder;
 
 import javax.persistence.Entity;
@@ -8,4 +9,17 @@ import javax.persistence.Entity;
 @Entity
 public class ProductTypeFiltering extends FilteringInformationHolder {
 
+
+    @FieldInfo(description = "will only return product types that are actively being used")
+    private boolean haveActualInstances;
+
+
+    public boolean isHaveActualInstances() {
+        return haveActualInstances;
+    }
+
+    public <T extends ProductTypeFiltering> T setHaveActualInstances(boolean haveActualInstances) {
+        this.haveActualInstances = haveActualInstances;
+        return (T) this;
+    }
 }
