9c4c8b8d52d9b67bccfe6ce92e1c3137496ad02a
asaf <asaf@flexi-core.com>
2020-03-25 11:39:35 +0200

    2.0.1 - moved ExternalServerFiltering from service to model

---
+++ src/main/java/com/flexicore/product/containers/request/ExternalServerFiltering.java
@@ -0,0 +1,11 @@
+package com.flexicore.product.containers.request;
+
+import com.flexicore.product.model.EquipmentFiltering;
+
+import javax.persistence.Entity;
+
+@Entity
+public class ExternalServerFiltering extends EquipmentFiltering {
+
+
+}
